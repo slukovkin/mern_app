a3b34d4ca5938eb71b2b1263e1533caedf9f3ed8
Sergey Lukovkin <slukovkin@icloud.com>
2022-11-03 21:48:41 +0200

    create now

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import jwt from "jsonwebtoken";
 
 export const checkAuth = async (req, res, next) => {
@@ -15,29 +14,3 @@ export const checkAuth = async (req, res, next) => {
     });
   }
 };
-=======
-import jwt from 'jsonwebtoken'
-
-export const checkAuth = (req, res, next) => {
-  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '')
-
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      
-      req.userId = decoded.id
-
-      next()
-      
-    } catch (error) {
-      res.json({
-        message: "Доступ закрыт"
-      })
-    } 
-  } else {
-    res.json({
-      message: "Доступ закрыт!"
-    })
-  }
-}
->>>>>>> 566c155128a27f318737975d1e72d0121c0c9d82
