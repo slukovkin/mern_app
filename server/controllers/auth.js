eea56e254ae82a8c14f9a8ee3c4c803586caeb25
Sergey Lukovkin <slukovkin@icloud.com>
2022-11-03 22:12:50 +0200

    create authorization

---
+++ server/controllers/auth.js
@@ -0,0 +1,59 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.json({
+      message: "Пользователь уже существует",
+    });
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(password, salt);
+
+  await User.create({ email: email, password: passwordHash });
+  res.json({
+    message: "Пользователь успешно создан",
+  });
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.json({
+        message: "Доступ запрещен",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+    res.json({
+      user,
+      token,
+      message: "Вы авторизированны",
+    });
+  } catch (error) {
+    res.json({
+      message: "Доступ запрещен",
+    });
+  }
+};
